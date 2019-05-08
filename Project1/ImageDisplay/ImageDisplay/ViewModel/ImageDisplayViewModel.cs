edad09a29245425169a654a126fa4a4163e8bfdb
Jonathan Markman <jmarkman@cleverdevices.com>
2019-05-08 17:22:00 -0400

    built out project 1 - added and built functional view and viewmodel - used codebehind binding for datacontext - talked about codebehind in doc - began talking about updatesourcetrigger

--- Project1/ImageDisplay/ImageDisplay/ViewModel/ImageDisplayViewModel.cs
+++ Project1/ImageDisplay/ImageDisplay/ViewModel/ImageDisplayViewModel.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace ImageDisplay.ViewModel
 {
-    public class ImageDisplayViewModel
+    public class ImageDisplayViewModel : INotifyPropertyChanged
     {
+        private string _imageUrl;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string ImageURL
+        {
+            get
+            {
+                return _imageUrl;
+            }
+            set
+            {
+                _imageUrl = value;
+                OnPropertyChanged(nameof(ImageURL));
+            }
+        }
+
+        public ImageDisplayViewModel()
+        {
+
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
