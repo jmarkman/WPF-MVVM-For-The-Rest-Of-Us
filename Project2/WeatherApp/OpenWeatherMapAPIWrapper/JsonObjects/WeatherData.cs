5efb865cfce6ece45c2e97b914f17bbde09f9138
Jonathan Markman <jmarkman@cleverdevices.com>
2019-08-13 17:21:05 -0400

    began building out second project - added API wrapper project to Project2 solution - included nuget reference to Newtonsoft.Json - created API wrapper class and JSON class

---
+++ Project2/WeatherApp/OpenWeatherMapAPIWrapper/JsonObjects/WeatherData.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWeatherMapAPIWrapper.JsonObjects
+{
+    public class WeatherData
+    {
+        [JsonProperty(PropertyName = "coord")]
+        public Coord Coordinates { get; set; }
+        [JsonProperty(PropertyName = "clouds")]
+        public Clouds CloudInfo { get; set; }
+        [JsonProperty(PropertyName = "weather")]
+        public Weather[] Weather { get; set; }
+        [JsonProperty(PropertyName = "main")]
+        public Main TemperatureAndPressure { get; set; }
+        [JsonProperty(PropertyName = "visibility")]
+        public int Visibility { get; set; }
+        [JsonProperty(PropertyName = "wind")]
+        public Wind WindInfo { get; set; }
+        [JsonProperty(PropertyName = "dt")]
+        public int UnixTimeWhenGathered { get; set; }
+        [JsonProperty(PropertyName = "sys")]
+        public Sys RegionInfo { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public int ID { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string CityName { get; set; }
+        [JsonProperty(PropertyName = "cod")]
+        public int ResponseCode { get; set; }
+        [JsonProperty(PropertyName = "message")]
+        public string ResponseMessage { get; set; }
+    }
+
+    public class Weather
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int WeatherConditionId { get; set; }
+        [JsonProperty(PropertyName = "main")]
+        public string WeatherParameters { get; set; }
+        [JsonProperty(PropertyName = "description")]
+        public string WeatherCondition { get; set; }
+        [JsonProperty(PropertyName = "icon")]
+        public string WeatherConditionIcon { get; set; }
+    }
+
+    public class Coord
+    {
+        [JsonProperty(PropertyName = "lon")]
+        public float Longitude { get; set; }
+        [JsonProperty(PropertyName = "lat")]
+        public float Latitude { get; set; }
+    }
+
+    public class Main
+    {
+        [JsonProperty(PropertyName = "temp")]
+        public float Temperature { get; set; }
+        [JsonProperty(PropertyName = "pressure")]
+        public int Pressure { get; set; }
+        [JsonProperty(PropertyName = "humidity")]
+        public int Humidity { get; set; }
+        [JsonProperty(PropertyName = "temp_min")]
+        public float MinimumTemperature { get; set; }
+        [JsonProperty(PropertyName = "temp_max")]
+        public float MaximumTemperature { get; set; }
+    }
+
+    public class Wind
+    {
+        [JsonProperty(PropertyName = "speed")]
+        public float WindSpeed { get; set; }
+        [JsonProperty(PropertyName = "deg")]
+        public float WindDirectionDegrees { get; set; }
+    }
+
+    public class Clouds
+    {
+        [JsonProperty(PropertyName = "all")]
+        public int CloudCoveragePercent { get; set; }
+    }
+
+    public class Sys
+    {
+        [JsonProperty(PropertyName = "country")]
+        public string CountryCode { get; set; }
+        [JsonProperty(PropertyName = "sunrise")]
+        public int SunriseTime { get; set; }
+        [JsonProperty(PropertyName = "sunset")]
+        public int SunsetTime { get; set; }
+    }
+}
---
+++ Project2/WeatherApp/OpenWeatherMapAPIWrapper/OpenWeatherMapClient.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using OpenWeatherMapAPIWrapper.JsonObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWeatherMapAPIWrapper
+{
+    public class OpenWeatherMapClient
+    {
+        /// <summary>
+        /// The API key that gives us permission to use the OpenWeatherMap API
+        /// </summary>
+        private readonly string apiKey;
+
+        /// <summary>
+        /// Static instance of <see cref="HttpClient"/> to interact with the OpenWeatherMap API
+        /// </summary>
+        private readonly static HttpClient httpClient = CreateAPIHttpClient();
+
+        public OpenWeatherMapClient(string apiKey)
+        {
+            this.apiKey = apiKey;
+        }
+
+        public async Task<WeatherData> GetWeatherDataForZipCodeAsync(string zipCode)
+        {
+            WeatherData weatherData;
+            string urlParams = $"weather?zip={zipCode},us&units=Imperial&appid={apiKey}";
+
+            var response = await httpClient.GetAsync(urlParams);
+            if (response.IsSuccessStatusCode)
+            {
+                var apiData = await response.Content.ReadAsStringAsync();
+                weatherData = JsonConvert.DeserializeObject<WeatherData>(apiData);
+            }
+            else
+            {
+                throw new InvalidOperationException($"The weather data for ZIP code '{zipCode}' could not be retrieved. {Environment.NewLine}API query status code: {response.StatusCode.ToString()}");
+            }
+
+            return weatherData;
+        }
+
+        private static HttpClient CreateAPIHttpClient()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/weather");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+    }
+}
